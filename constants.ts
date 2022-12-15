b70ecc3aaafc134ebdc6bc391d09094e532bde5d
Jonas Lekevicius <jonas@lekevicius.com>
2022-12-15 15:43:55 +0200

    Increased auction duraction a lot to better simulate fast searchers

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const AUCTION_DURATION = 8 * 1000 // 8 seconds, very generous for testing
+export const AUCTION_DURATION = 20 * 1000 // 20 seconds for a very slow searcher with remote RPC
 
 export const METHOD_RPC_NEW_AUCTION = 'nectar_auctionTransaction'
 export const METHOD_AUCTION_RESULT = 'nectar_auctionResult'
