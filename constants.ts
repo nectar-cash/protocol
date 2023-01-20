7c3f9e4465897f868dbf0ad862c7e8eed045c710
Jonas Lekevicius <jonas@lekevicius.com>
2023-01-20 15:28:23 +0200

    Shorter auction time

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const AUCTION_DURATION = 20 * 1000 // 20 seconds for a very slow searcher with remote RPC
+export const AUCTION_DURATION = 12 * 1000 // 12 seconds for a slow searcher with remote RPC
 
 export const METHOD_RPC_NEW_AUCTION = 'nectar_auctionTransaction'
 export const METHOD_AUCTION_RESULT = 'nectar_auctionResult'
